fb4ef42598e209a0afee0767d02320c612229ed5
KonstantinDenissov <konstantin.denissov@ua.pt>
2015-11-20 21:13:10 +0000

    tutorial CRUD

---
+++ Wikirials/Controllers/TutorialController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Wikirials.Models;
+using Wikirials.DAL;
+
+namespace Wikirials.Controllers
+{
+    public class TutorialController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Tutorial/
+        public ActionResult Index(string searchString)
+        {
+            var tutorial = from s in db.Tutorials
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                tutorial = tutorial.Where(s => s.Title.Contains(searchString));
+            }
+              
+            return View(tutorial);
+        }
+
+        // GET: /Tutorial/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tutorial tutorial = db.Tutorials.Find(id);
+            if (tutorial == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tutorial);
+        }
+
+        // GET: /Tutorial/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Tutorial/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Title,Body,Date,Classification,Type,ContentType")] Tutorial tutorial)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tutorials.Add(tutorial);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tutorial);
+        }
+
+        // GET: /Tutorial/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tutorial tutorial = db.Tutorials.Find(id);
+            if (tutorial == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tutorial);
+        }
+
+        // POST: /Tutorial/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Title,Body,Date,Classification,Type,ContentType")] Tutorial tutorial)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tutorial).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tutorial);
+        }
+
+        // GET: /Tutorial/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tutorial tutorial = db.Tutorials.Find(id);
+            if (tutorial == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tutorial);
+        }
+
+        // POST: /Tutorial/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tutorial tutorial = db.Tutorials.Find(id);
+            db.Tutorials.Remove(tutorial);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
